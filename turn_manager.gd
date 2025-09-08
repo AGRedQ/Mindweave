d18c6459bb51a612f5e6c720bb15e1a94a31529d
AGRedQ <dinhminhquan345@gmail.com>
2025-09-08 11:50:34 +0700

    reordering turnmanager files

--- turn_manager.gd
+++ turn_manager.gd
@@ -1,13 +1,19 @@
 extends Node2D
 
 var turn_queue: Array 	
+@onready var allies = $Actors/Allies
 
 
 func init_turn_queue():
-	turn_queue.append("player")
+	turn_queue.append("enemies")
 	
 func check_current_turn():
 	return turn_queue[0]
 	
+func next_turn():
+	turn_queue.pop_front()
+	
 func _ready() -> void:
 	init_turn_queue()
+	for child in allies.get_children():
+		print("Child: ",child.name.to_lower())
